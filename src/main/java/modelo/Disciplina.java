0d18ef0ad25f7f2056f5063f07c3f3cb366897c5
fagnerviana <fagsantiago@gmail.com>
2023-04-18 17:35:23 -0300

    Criando as classes modelos Disciplina e professor

---
+++ src/main/java/modelo/Disciplina.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import java.util.List;
+
+public class Disciplina {
+	
+	private int id_disciplina;
+	private String nome; //contabilizar em horas 30h 
+	private List<Professor> professor;
+	
+
+}
--- src/main/java/modelo/Professor.java
+++ src/main/java/modelo/Professor.java
@@ -1,13 +1,49 @@
 package modelo;
 
+import java.util.List;
+
 public class Professor extends Pessoa{
 	
 	private String cpf;
-
-	public Professor(String cpf) {
+	private List<Disciplina> disciplina;
+	
+	public Professor() {
+		
+	}
+	
+	
+	public Professor(String cpf, List<Disciplina> disciplina) {
 		super();
 		this.cpf = cpf;
+		this.disciplina = disciplina;
 	}
+
+
+	public Professor(int id, String nome, String email, TipoUsuario tipoUsuario) {
+		super(id, nome, email, tipoUsuario);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public List<Disciplina> getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(List<Disciplina> disciplina) {
+		this.disciplina = disciplina;
+	}
+	
 	
 	
 	
