7f192cc6eb0f5eb54efe54d1a429b4d77b96a26f
fagnerviana <fagsantiago@gmail.com>
2023-04-18 13:25:35 -0300

    Adicionado as dependencias necessarias para utilizar JAVAFX

--- src/main/java/modelo/Testando.java
+++ src/main/java/modelo/Testando.java
@@ -1,11 +1,20 @@
 package modelo;
 
-public class Testando {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Testando extends Application {
 
 	public static void main(String[] args) {
 		Aluno aluno = new Aluno();
-			
+			aluno.setNome("Fagner");
+			System.out.println(aluno);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
